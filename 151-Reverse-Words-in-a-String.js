d74385a150b60e377c29f040aeff32be39b845b4
Loay Ahmed <loayahmed304@gmail.com>
2024-08-09 11:59:33 +0300

    [151] [Time Beats: 99.37%] [Memory Beats: 88.99%]

---
+++ 151-Reverse-Words-in-a-String.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  return s
+    .trim()
+    .split(" ")
+    .reverse()
+    .filter((el) => el)
+    .join(" ");
+};
