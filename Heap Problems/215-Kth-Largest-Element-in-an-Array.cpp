6f9df603cf1fd782eb9bbec1974ff9b102ca194d
Loay Ahmed <loayahmed304@gmail.com>
2024-08-20 11:24:56 +0300

    [215] [Time Beats: 56.54%] [Memory Beats: 81.75%]

---
+++ Heap Problems/215-Kth-Largest-Element-in-an-Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        for(const auto& num: nums){
+            minHeap.push(num);
+            if(minHeap.size() > k){
+                minHeap.pop();
+            }
+        }
+
+        return minHeap.top();
+    }
+};
