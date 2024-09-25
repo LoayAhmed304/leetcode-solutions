50ee97bc5b8e250b00ac23a8f2103e6182af3da3
Loay Ahmed <loayahmed304@gmail.com>
2024-09-25 16:22:16 +0300

    [2416] [Time Beats: 27.25%] [Memory Beats: 32.07%]

---
+++ Daily Problems/September/26-2416-Sum-of-Prefix-Scores-of-Strings.py
@@ -0,0 +1,40 @@
+class PrefixNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.count = 0
+
+class PrefixTree:
+    def __init__(self):
+        self.root = PrefixNode()
+
+    def insert(self, w):
+        cur = self.root
+        for c in w:
+            i = ord(c) - ord('a')
+            if not cur.children[i]:
+                cur.children[i] = PrefixNode()
+            cur = cur.children[i]
+            cur.count += 1
+
+    def get_score(self, w):
+        cur = self.root
+        score = 0
+
+        for c in w:
+            i = ord(c) - ord('a')
+            cur = cur.children[i]
+            score += cur.count
+        return score
+
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        res = []
+        prefix_tree = PrefixTree()
+
+        for w in words:
+            prefix_tree.insert(w)
+        
+        for w in words:
+            res.append(prefix_tree.get_score(w))
+        
+        return res
