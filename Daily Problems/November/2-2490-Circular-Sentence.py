b0cdd4f6f5738e9d0c95b12e7ab19212e90df25e
Loay Ahmed <loayahmed304@gmail.com>
2024-11-03 12:23:19 +0200

    [2490] [Time Beats: 12.23%] [Memory Beats: 80.73%]

---
+++ Daily Problems/November/2-2490-Circular-Sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        cur  = ""
+        words = sentence.split(" ")
+        for i in range(len(words) - 1):
+            if words[i][-1] != words[i+1][0]:
+                return False
+        return words[len(words) - 1][-1] == words[0][0]
+            
