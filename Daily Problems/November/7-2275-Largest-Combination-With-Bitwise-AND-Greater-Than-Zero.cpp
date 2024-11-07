832898412a41a88fef215bcd3a52f0e041d6968a
Loay Ahmed <loayahmed304@gmail.com>
2024-11-07 12:59:04 +0200

    [2275] [Time Beats: 33.58%] [Memory Beats: 99.40%]

---
+++ Daily Problems/November/7-2275-Largest-Combination-With-Bitwise-AND-Greater-Than-Zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int res = INT_MIN;
+        for(int i = 0; i < 24; i++){
+            int cur = 0;
+            for(int j = 0; j < candidates.size(); j++)
+                if((candidates[j] & (1 << i)) != 0) cur++;
+            res = max(cur, res);
+        }
+        return res;
+    }
+};
