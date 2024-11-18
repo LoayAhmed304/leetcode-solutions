77aa09e9fd7895547d5e39cded2640aa86cd7605
Loay Ahmed <loayahmed304@gmail.com>
2024-11-18 16:51:12 +0200

    [1652] [Time Beats: 38.94%] [Memory Beats: 22.92%]

---
+++ Daily Problems/November/18-1652-Defuse-the-Bomb.py
@@ -0,0 +1,28 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        circ_code = code * 2
+        size = len(code)
+        def calc_sum(index, dir) -> int:
+            cur_sum = 0
+            loop_num = abs(k)
+            if dir > 0:
+                index += 1
+                for i in range(loop_num):
+                    cur_sum += code[index % size]
+                    index += 1
+            elif dir < 0:
+                index -= 1
+                for i in range(loop_num):
+                    cur_sum += code[index % size]
+                    index -= 1
+            return cur_sum
+                
+
+
+        res = []
+        for i in range(len(code)):
+            if k < 0:
+                res.append(calc_sum(i, -1))
+            else:
+                res.append(calc_sum(i, 1))
+        return res
